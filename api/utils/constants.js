32dc5a63484941b85817a7d08e41daddd1476f85
Shangni <shangnihu@gmail.com>
2019-05-31 16:35:51 -0400

    Added very light set up

---
+++ api/utils/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 5000,
+    mongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017/retro-api",
+    bodyLimit: "100kb",
+    SECRET: process.env.SECRET || "super-secret-passphrase"
+};
